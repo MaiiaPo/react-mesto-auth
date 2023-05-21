3379acac4466a2f075757a8d084674cd3f8871f0
Maiia <maiiapo.com@gmail.com>
2023-05-21 14:23:33 +0500

    Верстка форм регистрации и аутентификации.

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,7 +2,22 @@ import React from 'react';
 
 function Login() {
   return (
-    <div>Вход</div>
+    <div className="content auth">
+        <h1 className="auth__title">Вход</h1>
+        <form className="auth__form">
+          <input
+            className="auth__input"
+            type="email"
+            placeholder="Email"
+          />
+          <input
+            className="auth__input"
+            type="password"
+            placeholder="Пароль"
+          />
+        </form>
+        <button className="auth__submit">Войти</button>
+    </div>
   )
 }
 
--- src/components/Register.js
+++ src/components/Register.js
@@ -2,7 +2,23 @@ import React from 'react';
 
 function Register() {
   return (
-    <div>Регистрация</div>
+    <div className="content auth">
+      <h1 className="auth__title">Регистрация</h1>
+      <form className="auth__form">
+        <input
+          className="auth__input"
+          type="email"
+          placeholder="Email"
+        />
+        <input
+          className="auth__input"
+          type="password"
+          placeholder="Пароль"
+        />
+      </form>
+      <button className="auth__submit">Зарегистрироваться</button>
+      <p className="auth__sign-in">Уже зарегистрированы? Войти</p>
+    </div>
   )
 }
 
