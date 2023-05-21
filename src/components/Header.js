12c12d8a616f86547756a8e47e40a18af50221e5
Maiia <maiiapo.com@gmail.com>
2023-05-21 18:14:23 +0500

    Начальная верстка для header (для добавления регистрации и авторизации).

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,10 +4,13 @@ import headerLogo from "../images/logo.svg";
 function Header() {
   return (
     <header className="header">
-      <picture>
-        <source className="header__logo" media="(max-width: 767px)" srcSet={headerLogoMobile}/>
-        <img className="header__logo" src={headerLogo} alt="Логотип 'Место'"/>
-      </picture>
+      <div className="header__container">
+        <picture>
+          <source className="header__logo" media="(max-width: 767px)" srcSet={headerLogoMobile}/>
+          <img className="header__logo" src={headerLogo} alt="Логотип 'Место'"/>
+        </picture>
+        <div className="header__auth">Войти</div>
+      </div>
     </header>
   )
 }
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,9 @@
 import React from 'react';
+import {useForm} from "../hooks/useForm";
 
 function Login() {
+  const {values, handleChange, setValues} = useForm({})
+
   return (
     <div className="content auth">
         <h1 className="auth__title">Вход</h1>
@@ -9,11 +12,21 @@ function Login() {
             className="auth__input"
             type="email"
             placeholder="Email"
+            value={values.email || ""}
+            onChange={handleChange}
+            minLength="2"
+            maxLength="40"
+            required
           />
           <input
             className="auth__input"
             type="password"
             placeholder="Пароль"
+            value={values.password || ""}
+            onChange={handleChange}
+            minLength="6"
+            maxLength="200"
+            required
           />
         </form>
         <button className="auth__submit">Войти</button>
